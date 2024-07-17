3d672fd9442b153f4470668a46c2a08723af1d81
Gabriel Traiano <gabriel.traiano4@gmail.com>
2024-07-16 21:32:03 -0300

    CONTEXT PROCESSOR

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,8 +1,9 @@
 from home.models import CustomUser
 def proposal_status(request):
+    has_prop_wait = False
     if request.user.is_authenticated:
-        has_prop_wait = False
-        proposal = CustomUser.objects.filter(proposals_receive__status='waiting')
+        user = request.user
+        proposal = CustomUser.objects.filter(proposals_receive__status='waiting', id=user.id)
         if proposal:
             has_prop_wait = True
     
